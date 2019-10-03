74ea32fe1ea7d5e8666cdee9e872a8c122a82473
Ryan Ghaida <ryaboug@gmail.com>
2019-10-03 11:16:02 -0400

    started installing redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>text app coming soon...</p>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <p>text app coming soon...</p>
+      </div>
+    );
+  }
 }
 
 export default App;
