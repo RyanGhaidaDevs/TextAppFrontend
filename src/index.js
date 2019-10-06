ae7fd259b96e35b7451d0873c906819520f47bd7
Ryan Ghaida <ryaboug@gmail.com>
2019-10-06 13:19:28 -0400

    setting up redux and router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import signup from './components/signup'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Root from './components/Root';
+import { render } from 'react-dom'
+import { createStore } from 'redux'
 
-const Routing = (
-    <Router>
-      <div>
-        <Route exact path="/" component={signup} />
-        <Route path="/home" component={App} />
-      </div>
-    </Router>
-  )
+const store = createStore(rootReducer)
 
-ReactDOM.render(Routing, document.getElementById('root'));
+render(<Root store={store} />, document.getElementById('root'))
 
